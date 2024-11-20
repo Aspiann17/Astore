be58e72bf863aa0d06cf202eae4f526a6dcfcc47
Aspian <p.aspian1738@gmail.com>
2024-11-21 00:35:00 +0800

    Fragment

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ Thanks to [Ikmal](https://github.com/Malsoryz/)
 - [Material](https://m3.material.io/)
 - https://developer.android.com/develop/ui/views/theming/themes?hl=id
 ## Component
-- [Fragment](https://developer.android.com/guide/fragments/?hl=id)
+- Fragment
+  - https://youtu.be/m-PefJ2CcR8?si=8sCJHgxzU8-NQk_7
+  - https://developer.android.com/guide/fragments/?hl=id
 - [Tombol Aksi Mengambang](https://developer.android.com/develop/ui/views/components/floating-action-button?hl=id)
 - [TabLayout](https://developer.android.com/reference/com/google/android/material/tabs/TabLayout)
 - [Bottom Navigation](https://github.com/material-components/material-components-android/blob/master/docs/components/BottomNavigation.md)
--- app/src/main/java/id/my/aspian/astore/MainActivity.java
+++ app/src/main/java/id/my/aspian/astore/MainActivity.java
@@ -1,12 +1,8 @@
 package id.my.aspian.astore;
 
-import static id.my.aspian.astore.Utils.toast;
-
 import android.os.Bundle;
 import android.view.Window;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,9 +12,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,49 +32,53 @@ public class MainActivity extends AppCompatActivity {
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.blue));
         // end
 
+        // Fragment init
+        FrameLayout frame_layout = findViewById(R.id.main_frame);
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new HomeFragment()).commit();
+        // end
+
         // Bottom Navigation
         BottomNavigationView bottom_nav = findViewById(R.id.bottom_navigation);
         bottom_nav.setOnItemSelectedListener(item -> {
             int item_id = item.getItemId();
 
             if (item_id == R.id.nav_home) {
-                toast(this, "Home");
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new HomeFragment()).commit();
                 return true;
             } else if (item_id == R.id.nav_order) {
-                toast(this, "Cart");
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new OrderFragment()).commit();
                 return true;
             } else if (item_id == R.id.nav_profile) {
-                toast(this, "Profile");
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new ProfileFragment()).commit();
                 return true;
             }
 
             return false;
         });
-        // end
 
         // List Category
-        ListView list_category = findViewById(R.id.list_category);
-        list_category.setOnItemClickListener((parent, view, position, id) -> {
-            String category_id = ((TextView) view.findViewById(R.id.category_id)).getText().toString();
-            toast(this, category_id);
-        });
+//        ListView list_category = findViewById(R.id.list_category);
+//        list_category.setOnItemClickListener((parent, view, position, id) -> {
+//            String category_id = ((TextView) view.findViewById(R.id.category_id)).getText().toString();
+//            toast(this, category_id);
+//        });
         // end
 
-        tmp_data();
+//        tmp_data();
     }
 
-    public void tmp_data() {
-        ArrayList<HashMap<String, String>> list = new ArrayList<>();
-
-        for (int i = 1; i <= 10; i++) {
-            HashMap<String, String> map = new HashMap<>();
-            map.put("category_id", "" + i);
-            map.put("category_title", "Title" + i);
-            map.put("product_available", "0" + i);
-
-            list.add(map);
-        }
-
-        ((ListView) findViewById(R.id.list_category)).setAdapter(new SimpleAdapter(this, list, R.layout.list_category, new String[]{"category_id", "category_title", "product_available"}, new int[]{R.id.category_id, R.id.category_title, R.id.product_available}));
-    }
+//    public void tmp_data() {
+//        ArrayList<HashMap<String, String>> list = new ArrayList<>();
+//
+//        for (int i = 1; i <= 10; i++) {
+//            HashMap<String, String> map = new HashMap<>();
+//            map.put("category_id", "" + i);
+//            map.put("category_title", "Title" + i);
+//            map.put("product_available", "0" + i);
+//
+//            list.add(map);
+//        }
+//
+//        ((ListView) findViewById(R.id.list_category)).setAdapter(new SimpleAdapter(this, list, R.layout.list_category, new String[]{"category_id", "category_title", "product_available"}, new int[]{R.id.category_id, R.id.category_title, R.id.product_available}));
+//    }
 }
